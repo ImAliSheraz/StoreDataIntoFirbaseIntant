a5a91e1f67e66c96d34e76f330f9a6e5e7f9ba75
Ali Sheraz <alisheraz7777@gmail.com>
2020-04-10 07:17:24 -0700

    Initial commit

---
+++ app/src/main/java/com/buddytrainers/storedataintofirbaseintant/Persons.java
@@ -0,0 +1,47 @@
+package com.buddytrainers.storedataintofirbaseintant;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Persons implements Parcelable{
+
+    private Human humanData;
+
+    public Persons() {
+    }
+
+
+    protected Persons(Parcel in) {
+
+        humanData = in.readParcelable(Human.class.getClassLoader());
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(humanData, flags);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Persons> CREATOR = new Creator<Persons>() {
+        @Override
+        public Persons createFromParcel(Parcel in) {
+            return new Persons(in);
+        }
+
+        @Override
+        public Persons[] newArray(int size) {
+            return new Persons[size];
+        }
+    };
+
+    public Human getHumanData() {
+        return humanData;
+    }
+    public void setHumanData(Human humanData) {
+        this.humanData = humanData;
+    }
+}
